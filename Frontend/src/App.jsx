265894f581e51905b048ace9148bf8f56ecc3445
Sugam-Dev-25 <sugamkarmakar25@gmail.com>
2026-05-13 02:06:02 +0530

    All Fixed

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,5 @@
-import React from "react";
 import { useState } from "react";
 import { BrowserRouter } from "react-router-dom";
-// import "./App.css";
 import AllRoutes from "./AllRoutes";
 import TopHeader from "./Components/Layouts/TopHeader";
 import Header from "./Components/Layouts/Header";
